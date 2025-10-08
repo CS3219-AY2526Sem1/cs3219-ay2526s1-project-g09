11f0ac56c1d01685c8530afdb81723936f2f5cde
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-10-08 15:35:36 +0800

    Add docker for question-service

---
+++ backend-services/question-backend-service/eslint.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig(
+  {
+    ignores: ["eslint.config.mjs", "dist"],
+  },
+  {
+    /**
+     * Disabled for quick prototyping, but should be enabled later
+     * once the codebase is more stable.
+     */
+    rules: {
+      "no-unsafe-assignment": "off",
+      "no-unsafe-call": "off",
+      "no-unsafe-member-access": "off",
+      "no-explicit-any": "off",
+      "no-unsafe-return": "off",
+    },
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
