eb6b8df1b96e867a8ead7d3fded5f5d1423f8b61
wzhua02 <woozongghua@gmail.com>
2025-10-11 23:21:30 +0800

    Add public PublicRoute
    Purpose: Redirect logged in user back to matching page if they try to access pages like login page or sign up page.

--- ui-shell/src/App.tsx
+++ ui-shell/src/App.tsx
@@ -17,8 +17,9 @@ import HistoryPage from "@pages/history/HistoryPage";
 import QuestionAttemptsPage from "@pages/history/QuestionAttemptsPage";
 import AttemptPage from "@pages/history/AttemptPage";
 
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UnverifiedRoute from "./components/auth/UnverifiedRoute";
+import ProtectedRoute from "@components/auth/ProtectedRoute";
+import UnverifiedRoute from "@components/auth/UnverifiedRoute";
+import PublicRoute from "@components/auth/PublicRoute";
 
 import { useEffect } from "react";
 import { useAuth } from "@/data/UserStore";
@@ -38,10 +39,13 @@ export default function App() {
     <Routes>
       {/* Public access */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
-      <Route path="/resetPassword" element={<ResetPasswordPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      {/* Only non-logged in users*/}
+      <Route element={<PublicRoute />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
+        <Route path="/resetPassword" element={<ResetPasswordPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Route>
       {/* Only unverified users */}
       <Route element={<UnverifiedRoute />}>
         <Route path="/otp" element={<OtpPage />} />
