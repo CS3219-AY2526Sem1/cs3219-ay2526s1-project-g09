bf5054703a6bd983363972b8554c9f1e870d94b9
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-10-17 00:33:28 +0800

    Remove quesion and dbSchema for failedQuestion

--- backend-services/leetcode-backend-service/src/db/types/failedQuestion.ts
+++
@@ -1,8 +0,0 @@
-export interface FailedQuestion {
-  provider: string; // e.g., "leetcode"
-  leetcodeIndex: number;
-  attempts: number; // number of attempts made to fetch the question
-  lastTriedAt?: Date; // timestamp of the last attempt
-  createdAt?: Date;
-  updatedAt?: Date;
-}
