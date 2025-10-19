8f87284445e75b3634675e070af10b69a51140aa
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-10-19 10:42:37 +0800

    Set-up test configuration

---
+++ backend-services/leetcode-backend-service/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    include: ["tests/**/*.test.ts"],
+    globals: true,
+    setupFiles: ["tests/setup-env.ts"],
+    coverage: {
+      provider: "v8",
+      reportsDirectory: "./coverage",
+      reporter: ["text", "html", "lcov"],
+      exclude: ["**/tests/**", "**/dist/**"],
+    },
+  },
+});
