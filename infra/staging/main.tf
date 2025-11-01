10fd9c639e6f540f9e677d4cf7d6ecf438fb2a84
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-11-01 15:53:23 +0800

    Add s3 bucket for collab_ui frontend

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -17,3 +17,42 @@ terraform {
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+resource "aws_s3_bucket" "collab_ui_service" {
+  bucket = "peerprep-staging-collab-ui-service"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.collab_ui_service.id
+  policy = data.aws_iam_policy_document.allow_cloudfront_access.json
+}
+
+data "aws_iam_policy_document" "allow_cloudfront_access" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipal"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.collab_ui_service.arn}/*",
+    ]
+    # condition {
+
+    # }
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "frontend_cors" {
+  bucket = aws_s3_bucket.collab_ui_service.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag", "x-amz-meta-custom-header"]
+    max_age_seconds = 3600
+  }
+}
